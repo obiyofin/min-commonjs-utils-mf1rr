3fe2ef5649cd28d564cae6232b70c85510954ecc
obiyofin <goffxncamf@gmail.com>
2023-03-12 16:54:48 +0700

    add jest package and first testcase

--- test.js
+++
@@ -1,8 +0,0 @@
-const { add2 } = require("./index");
-const { printMessage } = require("./utils");
-
-//this is result of this
-let result = add2(2, 4, "bangkok is cool city");
-
-//console.log(2 times)
-printMessage(result);
