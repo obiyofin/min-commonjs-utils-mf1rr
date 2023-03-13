ac39474a7091c1aa8b94dfe7b6a059f5f20ed478
obiyofin <goffxncamf@gmail.com>
2023-03-13 11:01:43 +0700

    add fail test intentinally

--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
 const xx = require("./index");
 
 test("add 1 + 2 should equal 3", () => {
-  expect(xx.add2(1, 2, "bangkok is cool city")).toBe(3);
+  expect(xx.add2(1, 2, "bangkok is cool city")).toBe(4);
 });
